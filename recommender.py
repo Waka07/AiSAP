47667000c7d4b1d82ca60e3c98ac4f91e9997a00
Waka07 <118653322+Waka07@users.noreply.github.com>
2023-12-08 22:53:49 +0900

    コード修正

--- recommender.py
+++ recommender.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 import sklearn.metrics as skmt
-# import math
-from tqdm import tqdm
 
 def get_recommendations(searchReqList):
     # 受け取り手データの読み込み
     recipient_data = pd.read_csv('./data/recipient.csv')
+
     # 受け取り手タイプをチェックする
     recipient_type = 0
     for index, data in recipient_data.iterrows():
@@ -14,41 +13,40 @@ def get_recommendations(searchReqList):
             recipient_type = data["recipient_type"]
             break
 
-    print(recipient_type)
-
     # 評価データの読み込み
     rating_data = pd.read_csv('./data/rating.csv')
 
-    # item_id × recipient_typeの行列にする
+    # item_id × recipient_typeの行列を作成（とりあえずゼロで埋める）
     item_list = rating_data.sort_values('item_id').item_id.unique()
     recipient_list = rating_data.sort_values('recipient_type').recipient_type.unique()
-    rating_matrix_item = np.zeros([len(item_list), len(recipient_list)])
-    for item_id in tqdm(range(1, len(item_list)+1)):
-        user_list_item = rating_data[rating_data['item_id'] == item_id].sort_values('recipient_type').recipient_type.unique()
-        for type in user_list_item:
+    rating_matrix = np.zeros([len(item_list), len(recipient_list)])
+
+    # それぞれのitem_idとrecipient_typeの組み合わせの時の評価値を埋める
+    for item_id in range(1, len(item_list)+1):
+        recipient_list = rating_data[rating_data['item_id'] == item_id].sort_values('recipient_type').recipient_type.unique()
+        for type in recipient_list:
             try:
-                user_rate = rating_data[(rating_data['item_id'] == item_id) & (rating_data['recipient_type'] == type)].loc[:, 'rating']
+                rating = rating_data[(rating_data['item_id'] == item_id) & (rating_data['recipient_type'] == type)].loc[:, 'rating']
             except:
-                user_rate = -1
-            rating_matrix_item[item_id-1, type-1] = user_rate
+                rating = 0
+            rating_matrix[item_id-1, type-1] = rating
 
-    # NaNを0に変換
-    rating_matrix_item[pd.isna(rating_matrix_item)] = 0
+    # scikit-learnのpairwise_distancesを用いてコサイン類似度行列を計算
+    similarity_matrix = 1 - skmt.pairwise_distances(rating_matrix, metric='cosine')
 
-    # scikit-learnのpairwise_distancesを用いてコサイン類似度行列を出す
-    similarity_matrix = 1 - skmt.pairwise_distances(rating_matrix_item, metric='cosine')
     # 対角成分の値はゼロにする
     np.fill_diagonal(similarity_matrix, 0)
 
-    # 各ユーザの評価値を抜き出し「類似度×評価点」を算出
-    rating_matrix_user = rating_matrix_item[:, recipient_type - 1]
-    pred_rating_user = similarity_matrix * rating_matrix_user
-    # アイテム（行）ごとに「類似度×評価点」を合計
-    pred_rating_user = pred_rating_user.sum(axis=1)
+    # 対象の受け取り手タイプの評価値を抜き出し、「類似度×評価点」を算出
+    rating_matrix_recipient = rating_matrix[:, recipient_type - 1]
+    pred_rating = similarity_matrix * rating_matrix_recipient
 
-    #ここからレコメンドされたアイテムがどれだけあっていたかを評価していく
-    recommend_list = np.argsort(pred_rating_user)[::-1][:3] + 1
-    # print(recommend_list)
+    # 商品（行）ごとに「類似度×評価点」を合計
+    pred_rating_sum = pred_rating.sum(axis=1)
+    print(pred_rating_sum)
+
+    # 上位３商品をレコメンドリストに格納
+    recommend_list = np.argsort(pred_rating_sum)[::-1][:3] + 1
 
     # 商品情報を取得する
     item_data = pd.read_csv('./data/item.csv')
